361dd86c872842240b116795554783a88be3a105
Dean <deaneryfb@gmail.com>
2023-05-28 15:18:56 -0600

    The same  of (Promise = resolve and reject) but with async

--- node/1-basic/helpers/multiply.js
+++ node/1-basic/helpers/multiply.js
@@ -1,20 +1,20 @@
 const fs = require('node:fs');
 
-const createFile = (number = 5) => {
-
-    return new Promise((resolve, reject) => {
+const createFile = async (number = 5) => {
+    try {
         let exit = '';
         for (let index = 1; index <= 10; index++) {
             result = index * number;
             exit += `${number} x ${index} = ${result} \n`;
         }
+
         fs.writeFileSync(`table-${number}.txt`, exit);
 
-        resolve(`table-${number}.txt`);
+        return `table-${number}.txt`;
+    } catch (err) {
+        throw err;
+    }
 
-    });
 };
-
-
 
 module.exports = { createFile };// The only way in node.js if you want to import this function in another file
