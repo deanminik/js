4ee81733ed0c6c3f020fd411f36e8566bbaf03d1
Dean <deaneryfb@gmail.com>
2023-05-28 22:50:23 -0600

    yargs, for cli tools, flags, properties

--- node/1-basic/app.js
+++ node/1-basic/app.js
@@ -1,9 +1,29 @@
 
 const { createFile } = require('./helpers/multiply');
+const argv = require('yargs')
+                .option('b', {
+                    alias: 'base',
+                    type:'number',
+                    demandOption: true,
+                })
+                .check((argv, options) => {
+                    // console.log('yargs', argv)
+                    if(isNaN(argv.b)){
+                        throw 'The base must be a number'
+                    }
+                    return true;
+                })
+                .argv; 
 
-const number = r;
 
-createFile(number)
+// const number = 8;
+// console.log(process.argv);
+// console.log('base: yargs', argv.number);
+console.log(argv);
+
+createFile(argv.b)
     .then(fileName => console.log(fileName, 'created successfully'))
     .then(err => console.log(err));
+
+
 
