269a986d8bda803bca50cf33875bf8a1aab6c831
Dean <deaneryfb@gmail.com>
2023-05-30 02:00:07 -0600

    npm colors for cli elements

--- node/1-basic/app.js
+++ node/1-basic/app.js
@@ -1,12 +1,13 @@
 
 const { createFile } = require('./helpers/multiply');
 const argv = require('./config/yargs');
+const colors = require('colors');
 
 
 console.log(argv);
 
 createFile(argv.b, argv.l)
-    .then(fileName => console.log(fileName, 'created successfully'))
+    .then(fileName => console.log(fileName.rainbow, 'created successfully'))
     .then(err => console.log(err));
 
 
--- node/1-basic/helpers/multiply.js
+++ node/1-basic/helpers/multiply.js
@@ -1,11 +1,12 @@
 const fs = require('node:fs');
+const colors = require('colors');
 
 const createFile = async (number = 5, list = false) => {
     try {
         let exit = '';
         for (let index = 1; index <= 10; index++) {
             result = index * number;
-            exit += `${number} x ${index} = ${result} \n`;
+            exit += `${colors.green(number)} ${colors.bgMagenta('x')} ${index} = ${result} \n`;
         }
         if (list) {
             console.log(exit);
