18143358ba5fec97cb0a32240ec99b603b64ade9
Dean <deaneryfb@gmail.com>
2023-05-30 01:44:09 -0600

    Adding describe to our cli function

--- node/1-basic/app.js
+++ node/1-basic/app.js
@@ -1,30 +1,8 @@
 
 const { createFile } = require('./helpers/multiply');
-const argv = require('yargs')
-                .option('b', {
-                    alias: 'base',
-                    type:'number',
-                    demandOption: true,
-                })
-                .option('l', {
-                    alias: 'list',
-                    type:'boolean',
-                    demandOption: true,
-                    default: false
-                })
-                .check((argv, options) => {
-                    // console.log('yargs', argv)
-                    if(isNaN(argv.b)){
-                        throw 'The base must be a number'
-                    }
-                    return true;
-                })
-                .argv; 
+const argv = require('./config/yargs');
 
 
-// const number = 8;
-// console.log(process.argv);
-// console.log('base: yargs', argv.number);
 console.log(argv);
 
 createFile(argv.b, argv.l)
---
+++ node/1-basic/config/yargs.js
@@ -0,0 +1,26 @@
+const argv = require('yargs')
+    .option('b', {
+        alias: 'base',
+        type: 'number',
+        demandOption: true,
+        describe: 'Is the base of the multiply table'
+    })
+    .option('l', {
+        alias: 'list',
+        type: 'boolean',
+        demandOption: true,
+        default: false,
+        describe: 'show the list of the multiplication'
+
+    })
+    .check((argv, options) => {
+        // console.log('yargs', argv)
+        if (isNaN(argv.b)) {
+            throw 'The base must be a number'
+        }
+        return true;
+    })
+    .argv;
+
+
+module.exports = argv;
