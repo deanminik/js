abe33f1d99ebb1b3405e8d50abc2c4bdcb763f72
Dean <deaneryfb@gmail.com>
2023-07-01 00:30:17 -0600

    Adding CORS

--- node/7-RESTserver/models/server.js
+++ node/7-RESTserver/models/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 
 class Server {
@@ -15,6 +16,8 @@ class Server {
     }
 
     middlewares() {
+        //CORS
+        this.app.use(cors());
 
         // Public Directory 
         this.app.use(express.static('public')) //use() -> the key word to indicate this a middleware
