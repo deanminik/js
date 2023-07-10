a09eb3f7598fb568f94e107a20e1edeac66a657a
Dean <deaneryfb@gmail.com>
2023-07-09 19:08:53 -0600

    validate email, status and password

--- node/7-RESTserver/controllers/auth.controller.js
+++ node/7-RESTserver/controllers/auth.controller.js
@@ -1,9 +1,52 @@
 const { response } = require('express');//To help us with the typing->tipado 
+const User = require('../models/user');
+const bcryptjs = require('bcryptjs');
 
-const login = (req, res = response ) =>{
-    res.json({
-        msg:'Login ok'
-    })
+const login = async (req, res = response) => {
+
+    const { email, password } = req.body;
+    try {
+
+        //is the email exists?
+        const user = await User.findOne({ email }); //Remember, our user model has a property called email
+        if (!user) {
+            return res.status(400).json({
+                msg: 'User / Password are not correct - email'
+            });
+        }
+
+        //is the user active?
+        //this-> if(user.state === false) and this -> !user.state are the same 
+        if (!user.state) {
+            return res.status(400).json({
+                msg: 'User / Password are not correct - state: false'
+            });
+        }
+
+
+        //verify password
+        const validPassword = bcryptjs.compareSync(password, user.password);
+        if(!validPassword){
+            return res.status(400).json({
+                msg: 'User / Password are not correct - password: false'
+            });
+        }
+
+
+        //Is everything is correct generate the JWT
+
+        res.json({
+            msg: 'Login ok',
+            email, password
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg: 'status: 500 internal server error | There is something wrong'
+        })
+    }
+
 }
 
 module.exports = {
