f39555b1faf446a6cee0d7e0113c14efacbcf3c1
Dean <deaneryfb@gmail.com>
2023-09-24 16:05:46 -0600

    Initial 5 endpoints of category

--- node/7-RESTserver/routes/categories.route.js
+++ node/7-RESTserver/routes/categories.route.js
@@ -9,8 +9,28 @@ const router = Router();
  * {{url}}/api/categories
  */
 
+// Endpoint to get all categories - Public 
 router.get('/',(req, res) => {
-    res.json("This is just to test our route | everything ok");
-})
+    res.json('this is a get');
+});
 
+//Endpoint to get just one category by id - Public
+router.get('/:id',(req, res) => {
+    res.json('this is a get by ID');
+});
+
+//Endpoint to create a new category - private -> Only someone with a valid token 
+router.post('/',(req, res) => {
+    res.json('this is a post');
+});
+
+//Endpoint to update a register using an id - private -> Only someone with a valid token 
+router.put('/:id',(req, res) => {
+    res.json('this is a put');
+});
+
+//Endpoint to delete a category using an id - Only Admin can call this endpoint 
+router.delete('/:id',(req, res) => {
+    res.json('this is a delete -> we do not delete, just update the state ');
+});
 module.exports = router;
