3bcc0d61b8e84534537411241c4bced68a199bfb
Dean <deaneryfb@gmail.com>
2023-09-24 20:26:52 -0600

    Adding Category Model | Also an index js to call in a better way all models

---
+++ node/7-RESTserver/models/category.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const CategorySchema = Schema({
+    //name-> has to be the same name from the collection in mongo 
+    name: {
+        type: String,
+        required: [true, 'The name is required']
+    },
+    state: {
+        type: Boolean,
+        default: true,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+
+
+});
+
+
+module.exports = model('Category', CategorySchema);
