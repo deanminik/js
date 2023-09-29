7cfc01240ab144568cc0e88f261900b13b952fbc
Dean <deaneryfb@gmail.com>
2023-09-28 20:18:37 -0600

    Fill out the Model file

--- node/7-RESTserver/models/index.js
+++ node/7-RESTserver/models/index.js
@@ -2,6 +2,7 @@ const Category = require('./category');
 const Role = require('./role');
 const Server = require('./server');
 const User = require('./user');
+const Product = require('./product');
 
 
 
@@ -9,5 +10,6 @@ module.exports = {
     Category,
     Role,
     Server,
-    User
+    User,
+    Product
 }
---
+++ node/7-RESTserver/models/product.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const ProductSchema = Schema({
+    //name-> has to be the same name from the collection in mongo 
+    name: {
+        type: String,
+        required: [true, 'The name is required'],
+        unique: true
+    },
+    state: {
+        type: Boolean,
+        default: true,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    category: {
+        type: Schema.Types.ObjectId,// To make reference which category belongs | useful is we forget to send the category this sends a msg
+        ref: 'Category',
+        required: true
+    },
+    description: { type: String },
+    available: { type: Boolean, default: true },
+});
+
+ProductSchema.methods.toJSON = function () {
+    //It is important to user function instead () => arrow function, because we need to use the object "this"
+    // the arrow function keep "this" out side of itself. So use "function" to make a reference to the instance created 
+
+    const { __v, state, ...data } = this.toObject();
+    return data;
+
+    /**
+     * what is this? __v, state
+     * I am taking __v, state outside and the all variables like name,  etc are saving inside the new variable called ...data 
+     */
+
+
+}
+
+module.exports = model('Product', ProductSchema);
