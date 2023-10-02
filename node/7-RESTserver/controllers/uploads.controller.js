d27037ececfc9c7b224efb6370422a2a79dd2c73
Dean <deaneryfb@gmail.com>
2023-10-01 19:00:37 -0600

    Upload files

--- node/7-RESTserver/controllers/uploads.controller.js
+++ node/7-RESTserver/controllers/uploads.controller.js
@@ -1,9 +1,39 @@
 const { response } = require('express');//To help us with the typing->tipado 
-
+const path = require('path');
 
 
 const loadFiles = (req, resp = response) => {
-    resp.json({ msg: 'Load Files controller' });
+
+    // console.log(req.files);
+
+    // resp.json({ msg: 'Load Files controller' });
+
+
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+        resp.status(400).json({ msg: 'No files were uploaded.' });
+        return;
+    }
+
+    if (!req.files.file) {
+        resp.status(400).json({ msg: 'No files were uploaded.' });
+        return;
+    }
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    const { file } = req.files;
+    const uploadPath = path.join(__dirname, '../uploads/', file.name);
+
+    // Use the mv() method to place the file somewhere on your server
+    file.mv(uploadPath, (err) => {
+        if (err) {
+            console.log(err);
+            return resp.status(500).json({ err });
+        }
+
+
+        resp.json({ msg: 'File uploaded!' + uploadPath });
+    });
 }
 
 module.exports = {
--- node/7-RESTserver/models/server.js
+++ node/7-RESTserver/models/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('../database/config');
+const fileUpload = require('express-fileupload');
 
 
 class Server {
@@ -8,14 +9,14 @@ class Server {
         //Variables
         this.app = express();
         this.port = process.env.PORT;
-        
+
         this.paths = {
-            auth:       '/api/auth',
-            users:      '/api/users',
+            auth: '/api/auth',
+            users: '/api/users',
             categories: '/api/categories',
-            products:   '/api/products',
-            search:     '/api/search',
-            uploads:    '/api/uploads'
+            products: '/api/products',
+            search: '/api/search',
+            uploads: '/api/uploads'
         }
 
         // this.userPath = '/api/users';
@@ -35,7 +36,7 @@ class Server {
         await dbConnection();
     }
 
-    middlewares() {
+    middlewares() {//This executes before our routes section 
         //CORS
         this.app.use(cors());//use() -> the key word to indicate this is a middleware
 
@@ -49,6 +50,13 @@ class Server {
 
         // Public Directory 
         this.app.use(express.static('public')) //use() -> the key word to indicate this is a middleware
+
+        //Middleware from npm express-upload 
+        //This is to load files
+        this.app.use(fileUpload({
+            useTempFiles: true,
+            tempFileDir: '/tmp/'
+        }));
     }
 
     // routes() {
@@ -66,7 +74,7 @@ class Server {
         this.app.use(this.paths.products, require('../routes/products.route'));
         this.app.use(this.paths.search, require('../routes/search.route'));
         this.app.use(this.paths.uploads, require('../routes/uploads.route'));
-        
+
     }
 
 
