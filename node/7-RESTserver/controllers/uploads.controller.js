0d4df27665b80d097ac953f7250472ae5e012d12
Dean <deaneryfb@gmail.com>
2023-10-01 21:51:51 -0600

    Validate extensions

--- node/7-RESTserver/controllers/uploads.controller.js
+++ node/7-RESTserver/controllers/uploads.controller.js
@@ -22,18 +22,37 @@ const loadFiles = (req, resp = response) => {
 
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
     const { file } = req.files;
-    const uploadPath = path.join(__dirname, '../uploads/', file.name);
 
-    // Use the mv() method to place the file somewhere on your server
-    file.mv(uploadPath, (err) => {
-        if (err) {
-            console.log(err);
-            return resp.status(500).json({ err });
-        }
+    const splitFileName = file.name.split('.');
+    const extensionOfTheFile = splitFileName[splitFileName.length - 1];
+
+    // console.log(splitFileName);
+
+    //Validate extension 
+    const validatedExtension = ['png', 'jpg', 'jpeg', 'gif'];
+
+    if (!validatedExtension.includes(extensionOfTheFile)) {
+
+        return resp.status(400).json({
+            msg: `The extension: ${extensionOfTheFile} is not allowed. These are the available extensions: ${validatedExtension} `
+        });
+    }
+
+    resp.json({ extensionOfTheFile });
 
 
-        resp.json({ msg: 'File uploaded!' + uploadPath });
-    });
+    // const uploadPath = path.join(__dirname, '../uploads/', file.name);
+
+    // // Use the mv() method to place the file somewhere on your server
+    // file.mv(uploadPath, (err) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return resp.status(500).json({ err });
+    //     }
+
+
+    //     resp.json({ msg: 'File uploaded!' + uploadPath });
+    // });
 }
 
 module.exports = {
