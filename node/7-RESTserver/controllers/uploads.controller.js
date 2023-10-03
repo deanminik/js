45e24a2b7035785054adcc25d4e6e84b70fb7490
Dean <deaneryfb@gmail.com>
2023-10-03 01:57:48 -0600

    Update user image

--- node/7-RESTserver/controllers/uploads.controller.js
+++ node/7-RESTserver/controllers/uploads.controller.js
@@ -2,6 +2,7 @@ const { response } = require('express');//To help us with the typing->tipado
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const { uploadFile } = require('../helpers');
+const { User, Product } = require('../models');
 
 
 
@@ -32,8 +33,38 @@ const loadFiles = async (req, resp = response) => {
 
 const updateImage = async (req, resp = response) => {
     const { id, collection } = req.params;
+    let model;
+    //Here we validated the users or products 
+    switch (collection) {
+        case 'users':
+            model = await User.findById(id);
+            if (!model) {
+                return resp.status(400).json({
+                    msg: `There is not a user with this ID: ${id} `
+                });
+            }
 
-    resp.json({id, collection})
+            break;
+        case 'products':
+            model =  await Product.findById(id);
+            if (!model) {
+                return resp.status(400).json({
+                    msg: `There is not a product with this ID: ${id} `
+                });
+            }
+
+            break;
+
+        default:
+            return resp.status(500).json({ msg: 'I forgot to validate this' });
+    }
+
+    const name = await uploadFile(req.files, undefined, collection);
+    model.img = name;
+
+    await model.save();
+
+    resp.json(model);
 }
 
 module.exports = {
