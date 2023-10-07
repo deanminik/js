78c83c5af7490cfa5c195c425797a3097d5beeac
Dean <deaneryfb@gmail.com>
2023-10-07 15:11:51 -0600

    Client side - Messages of connections and disconnect

--- node/8-web-socket-server/public/js/socket-client.js
+++ node/8-web-socket-server/public/js/socket-client.js
@@ -1,1 +1,18 @@
+//REFERENCE OG HTML 
+const lblOnline  = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
 const socket_of_a_client = io();
+
+//These are listeners
+socket_of_a_client.on('connect',() =>{
+     console.log("I'm a client and I'm connected");
+     lblOffline.style.display = 'none';
+     lblOnline.style.display = '';
+});
+
+socket_of_a_client.on('disconnect',() =>{
+console.log("I'm a client and I'm disconnected");
+lblOnline.style.display = 'none';
+lblOffline.style.display = '';
+});
