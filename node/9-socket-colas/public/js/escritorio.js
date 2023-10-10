9224a5edd999a677e50f342959efac7005838343
Dean <deaneryfb@gmail.com>
2023-10-09 22:27:56 -0600

    Preparing desktop screen

--- node/9-socket-colas/public/js/escritorio.js
+++ node/9-socket-colas/public/js/escritorio.js
@@ -1,3 +1,44 @@
+//REFERENCE HTML 
+const lblDesktop = document.querySelector('h1');
+const btn = document.querySelector('button');
+
+const searchParams = new URLSearchParams(window.location.search);
+
+if (!searchParams.has('escritorio')) {
+    window.location = 'index.html';
+    throw new Error('The desktop is mandatory');
+}
+
+const desktop = searchParams.get('escritorio');
+lblDesktop.innerText = desktop;
+// console.log({desktop});
+
+const socket = io();
 
 
-console.log('Escritorio HTML');
+
+socket.on('connect', () => {
+
+    btn.disabled = false;
+
+});
+
+socket.on('disconnect', () => {
+    btn.disabled = true;
+
+});
+
+
+socket.on('last-ticket', (lastTicket) => {
+    // lblNuevoTicket.innerText = 'Ticket ' + lastTicket;
+});
+
+
+btn.addEventListener('click', () => {
+
+    // socket.emit('next-ticket', null, (ticket) => {
+    //     // console.log('From the server', ticket);
+    //     lblNuevoTicket.innerText = ticket;
+    // });
+
+});
