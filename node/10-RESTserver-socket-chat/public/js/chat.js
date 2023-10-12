c7cb5ba3e5cae0684863bd27792a4c9e902e5a85
Dean <deaneryfb@gmail.com>
2023-10-11 20:36:13 -0600

    Validate token Front End

--- node/10-RESTserver-socket-chat/public/js/chat.js
+++ node/10-RESTserver-socket-chat/public/js/chat.js
@@ -1,3 +1,41 @@
-//Add the instance of the io 
 
-const socket = io();
+const myURL = (window.location.hostname.includes('localhost'))
+    ? 'http://localhost:8079/api/auth/'
+    : '<domain/api/auth/>'
+
+
+let user = null;
+let socket = null;
+
+//validate the token from the local storage 
+const validateJWT = async () => {
+
+    const token = localStorage.getItem('token') || '';
+
+    if (token.length <= 10) {
+        window.location = 'index.html';
+        throw new Error('There is not token in the server');
+    }
+
+    const resp = await fetch(myURL, {
+        headers: { 'x-token': token }
+    });
+
+    const { user: userDB, token: tokenDB } = await resp.json();
+    console.log(userDB, tokenDB);
+   
+    localStorage.setItem('token', tokenDB); //-> this is if you want renovate the token, for example extending its time 
+    user = userDB;
+};
+
+const main = async () => {
+
+
+    await validateJWT();
+
+}
+
+main();
+
+//Add the instance of the io 
+// const socket = io();
