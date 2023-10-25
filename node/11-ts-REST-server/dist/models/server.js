ec5967941b5c0661c2b9e0c74c4215f32bbda518
Dean <deaneryfb@gmail.com>
2023-10-25 00:11:51 -0600

    necessary Middlewares

--- node/11-ts-REST-server/dist/models/server.js
+++ node/11-ts-REST-server/dist/models/server.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const user_route_1 = __importDefault(require("../routes/user.route"));
+const cors_1 = __importDefault(require("cors"));
 class Server {
     constructor() {
         this.apiPaths = {
@@ -12,8 +13,17 @@ class Server {
         };
         this.app = (0, express_1.default)();
         this.port = process.env.PORT || '8001';
-        //Define my routes
+        this.middlewares();
         this.routes();
+    }
+    middlewares() {
+        //Just functions before to executes our routes 
+        //CORS:
+        this.app.use((0, cors_1.default)());
+        //Parse the body to read it
+        this.app.use(express_1.default.json()); //So with this, express parse for me, the body when we send in the request post, put, delete etc 
+        //Public directory -> To share static content Example: web site 
+        this.app.use(express_1.default.static('public')); //public -> /11-ts-REST-server/public/
     }
     routes() {
         this.app.use(this.apiPaths.users, user_route_1.default);
--- node/11-ts-REST-server/models/server.ts
+++ node/11-ts-REST-server/models/server.ts
@@ -1,5 +1,6 @@
 import express, { Application } from 'express';
 import userRoutes from '../routes/user.route';
+import cors from 'cors';
 
 class Server {
 
@@ -12,9 +13,26 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '8001';
-
-        //Define my routes
+        
+        this.middlewares();
+  
         this.routes();
+
+    }
+    middlewares(){
+        //Just functions before to executes our routes 
+
+        //CORS:
+        this.app.use(cors());
+
+        //Parse the body to read it
+        this.app.use(express.json()); //So with this, express parse for me, the body when we send in the request post, put, delete etc 
+        
+
+        //Public directory -> To share static content Example: web site 
+        this.app.use(express.static('public')) //public -> /11-ts-REST-server/public/
+
+
 
     }
 
