4c805bf3bc9aa843e1963f942e954aa62ac2c996
Dean <deaneryfb@gmail.com>
2023-11-27 00:09:48 -0600

    Types | Objects | functions

--- typescript/1-exploring/app.js
+++ typescript/1-exploring/app.js
@@ -1,8 +1,1 @@
 "use strict";
-const msg = 'Hello Dean';
-console.log(msg);
-const hero = {
-    name: 'Iroman',
-    age: 45
-};
-hero.age = '10';
--- typescript/1-exploring/app.ts
+++ typescript/1-exploring/app.ts
@@ -1,10 +1,1 @@
-const msg: string = 'Hello Dean';
 
-console.log(msg);
-
-const hero = {
-    name:'Iroman',
-    age:45
-}
-
-hero.age = '10';
